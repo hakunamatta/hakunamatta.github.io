32ff9597eb0068a736a3fc018010a3212752b504
plastyrek <yuraorgish@gmail.com>
2019-05-24 14:55:49 +0300

    add canvas, random generation

---
+++ js/drawCanvas.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.textAlign = "center";
+ctx.fillText("ТУТ МОГЛА БЫТЬ ВАША РЕКЛАМА", canvas.width/2, canvas.height/2);
--- js/monteCarlo.js
+++ js/monteCarlo.js
@@ -1,9 +1,22 @@
-function generateRandomValue(min,max){
-  return Math.random() * (max - min) + min;
+function generateRandomValue(min,experimentsCount){
+  const uniqueNumber = [];
+
+  for(var i=0;uniqueNumber.length < experimentsCount;i++){
+    const randomNumber = Math.floor((Math.random() * experimentsCount - min) - min);
+
+    if (uniqueNumber.indexOf(randomNumber) == -1) {
+
+        uniqueNumber.push(randomNumber);
+
+    }
+  }
+
+  return uniqueNumber;
 }
 
 function changeInputValue(){
   const input = document.getElementById('inputCont').value;
 
-  console.log(generateRandomValue(0,input));
+  console.log(generateRandomValue(1,input));
+
 }
