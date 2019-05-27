8d013214a223f99015b534bd958218de752ec278
yury_orgish <yury_orgish@EPBYBREW0031.minsk.epam.com>
2019-05-27 15:26:45 +0300

    dodelati doma

--- js/drawCanvas.js
+++ js/drawCanvas.js
@@ -1,4 +1,5 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.textAlign = "center";
-ctx.fillText("ТУТ МОГЛА БЫТЬ ВАША РЕКЛАМА", canvas.width/2, canvas.height/2);
+ctx.beginPath();
+ctx.arc(canvas.width/2, 75, 15/2*Math.PI, 0, 2 * Math.PI);
+ctx.stroke();
--- js/monteCarlo.js
+++ js/monteCarlo.js
@@ -1,5 +1,12 @@
 function generateRandomValue(min,experimentsCount){
+  // var canvas = document.getElementById("canvas");
+  // var ctx = canvas.getContext("2d");
   const uniqueNumber = [];
+  //       Scanv = canvas.heigth*canvas.width;
+  //
+  // var   Sresult;
+  //       n;
+  //       N;
 
   for(var i=0;uniqueNumber.length < experimentsCount;i++){
     const randomNumber = Math.floor((Math.random() * experimentsCount - min) - min);
@@ -9,7 +16,10 @@ function generateRandomValue(min,experimentsCount){
         uniqueNumber.push(randomNumber);
 
     }
+
   }
+
+
 
   return uniqueNumber;
 }
